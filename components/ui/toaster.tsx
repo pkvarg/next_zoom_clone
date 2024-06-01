e022f81cc3178c4c50a2ec4d7d7234b052b15e7e
peter varga <pkvarg@yahoo.se>
2024-06-01 21:46:14 +0200

    toaster

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import {
   Toast,
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from '@/components/ui/toast'
+import { useToast } from '@/components/ui/use-toast'
 
 export function Toaster() {
   const { toasts } = useToast()
@@ -17,8 +17,12 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
+          <Toast
+            key={id}
+            {...props}
+            className='border-none bg-dark-1 text-white'
+          >
+            <div className='grid gap-1 '>
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
